3070b13c9c90c4a727e21dd06e94246e6285ba0e
lom <lishalom@sina.com>
2020-07-23 21:48:13 +0800

    Update answers.js

--- answers.js
+++ answers.js
@@ -34,53 +34,54 @@ module.exports = {
                 args: ['--no-sandbox']
             });
             const page = await browser.newPage();
+            try {
+                await page.goto(this.link(req.param('question')));
 
-            await page.goto(this.link(req.param('question')));
+                await page.addScriptTag({path: './jquery.js'});
 
-            await page.addScriptTag({path: './jquery.js'});
-
-            await page.evaluate(() => {
-                window.scrollTo(0, 1500)
-            });
-
-            await page.waitFor(3000);
-
-            await page.evaluate(() => {
-                $('.ui_section_footer').parent().click();
-            })
-
-            await page.waitFor(3000);
-
-            await page.evaluate(() => {    //点击【阅读回答】按钮
-                $("[class='q-text qu-flex--auto puppeteer_test_button_text qu-medium']").each(function () {
-                    $(this).click()
+                await page.evaluate(() => {
+                    window.scrollTo(0, 1500)
                 });
-            });
 
-            await page.waitFor(5000);
+                await page.waitFor(3000);
 
-            let answers = await page.evaluate(() => {
-                let answers = new Array();
-                $("[class='q-box qu-pt--medium qu-pb--medium']").each(function (index, item) {
-                    let user = $(this).find('.qu-alignSelf--center').find('a').eq(0).text();
-                    let content = $(this).find('.qu-userSelect--text').html();
-                    if (user !== undefined && content !== undefined) {
-                        answers[index] = {};
-                        answers[index].user_name = user;    //用户昵称
-                        answers[index].user_link = $(this).find('.qu-alignSelf--center').find('a').attr('href');    //用户主页地址
-                        answers[index].user_credential = $(this).find('.qu-alignSelf--center').find('.qu-borderWidth--retinaOverride').text();     // 用户认证信息
-                        answers[index].user_avatar = $(this).find('img').attr('src');     // 用户头像
-                        answers[index].user_info = '';     // 用户住址等信息
-                        answers[index].content = content     // 回答内容
-                    }
+                await page.evaluate(() => {
+                    $('.ui_section_footer').parent().click();
+                })
+
+                await page.waitFor(3000);
+
+                await page.evaluate(() => {    //点击【阅读回答】按钮
+                    $("[class='q-text qu-flex--auto puppeteer_test_button_text qu-medium']").each(function () {
+                        $(this).click()
+                    });
                 });
-                return answers;
-            });
 
-            await browser.close();
+                await page.waitFor(5000);
 
-            data.answers(answers, req.param('question_id'))
-
+                let answers = await page.evaluate(() => {
+                    let answers = new Array();
+                    $("[class='q-box qu-pt--medium qu-pb--medium']").each(function (index, item) {
+                        let user = $(this).find('.qu-alignSelf--center').find('a').eq(0).text();
+                        let content = $(this).find('.qu-userSelect--text').html();
+                        if (user !== undefined && content !== undefined) {
+                            answers[index] = {};
+                            answers[index].user_name = user;    //用户昵称
+                            answers[index].user_link = $(this).find('.qu-alignSelf--center').find('a').attr('href');    //用户主页地址
+                            answers[index].user_credential = $(this).find('.qu-alignSelf--center').find('.qu-borderWidth--retinaOverride').text();     // 用户认证信息
+                            answers[index].user_avatar = $(this).find('img').attr('src');     // 用户头像
+                            answers[index].user_info = '';     // 用户住址等信息
+                            answers[index].content = content     // 回答内容
+                        }
+                    });
+                    return answers;
+                });
+                await browser.close();
+                data.answers(answers, req.param('question_id'))
+            } catch (e) {
+                console.log(e)
+                await browser.close();
+            }
         })();
 
         jsonWrite(res, true);
